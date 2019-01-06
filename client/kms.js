89699a3b68a76686f4f1eb556ebf14563ca1a09f
Ron-Raz <ron.raz@US-RONR-MAC.local>
2019-01-05 23:02:03 -0500

    20190105-2302

--- client/kms.js
+++ client/kms.js
@@ -1,36 +1,48 @@
-const VER = '20190104-1807';
+const VER = '20190105-2302';
 
 const myDebug = true;
 
 if (myDebug === false) {
-	console.log = function () { };
+	console.log = function () {};
 }
 
 var selectors = {
+	// upload media
 	'sipAction': "#customdata-ServerAction-label, #edit_entry > div:nth-child(14),",
 	'sipEndpoint': "#customdata-SIP, #customdata-SIP+P, #customdata-SIP-label, #entry-metadata,",
 	'sipStats': "#customdata-Stats-label, #edit_entry > div:nth-child(16),",
+	'mediaPanel': "#wrapper,",
+	// SIP admin page, view mode
 	'entryBlock': "#entryDataBlock,",
-	'mediaPanel':"#wrapper,"
+	'panels': "#entry-metadata > dt:nth-child(1), #mySidebar, #eCaptions, #stats_wrap",
+	'endpoint': "#entry-metadata > dt:nth-child(1), #entry-metadata > dd:nth-child(2), ",
+	'actions': "#entryActionsMenu > li:nth-child(2), #entryActionsMenu > li:nth-child(3), #entryActionsMenu > li.divider, #entryActionsMenu > li:nth-child(5)"
+
+
+
 };
+
+function getSelectors(selarray) {
+	var combinedSelectors = '';
+	for (i in selarray) combinedSelectors += selectors[selarray[i]];
+	// remove last comma
+	combinedSelectors = combinedSelectors.slice(0, -1);
+	return combinedSelectors;
+}
+
 function addCSSRule(sheet, selectorsArray, rules, index) {
 	// addCSSRule(document.styleSheets[0], "header", "float: left");
 	// selectorsArray is an array from selectors
-	var combinedSelectors = '';
-	for (i in selectorsArray) combinedSelectors += selectors[selectorsArray[i]];
-	// remove last comma
-	combinedSelectors= combinedSelectors.slice(0, -1);
 	if ("insertRule" in sheet) {
-		sheet.insertRule(combinedSelectors + "{" + rules + "}", index);
-	}
-	else if ("addRule" in sheet) {
-		sheet.addRule(combinedSelectors, rules, index);
+		sheet.insertRule(getSelectors(selectorsArray) + "{" + rules + "}", index);
+	} else if ("addRule" in sheet) {
+		sheet.addRule(getSelectors(selectorsArray), rules, index);
 	}
 }
 
 $(function () {
 	var wlp = window.location.pathname;
-	console.log("client/kms.js version=", VER, "wlp=",wlp);
+	console.log("client/kms.js version=", VER, "wlp=", wlp);
 	/*
 	pathname		action
 	========		======
@@ -45,16 +57,14 @@ $(function () {
 		addCSSRule(document.styleSheets[0], ['sipAction', 'sipStats'], "display: none !important");
 		console.log("only hide sipAction and SipStats");
 	} else {
-		/*
-		if (wlp.startsWith('/media/')) {
+		if (wlp.startsWith('/media/SIP/')) {
 			console.log('sip admin page')
-			//addCSSRule(document.styleSheets[0], ['entryBlock'], "width: 100% !important");
-			//$("#wrapper, #mySidebar, #stats_wrap, #entryActions, #entry-nav").detach();
+			addCSSRule(document.styleSheets[0], ['entryBlock'], "width: 100% !important");
+			$("#wrapper, #mySidebar, #stats_wrap, #entryActions, #entry-nav").detach();
 		} else {
 			console.log("rule added to hide sip field");
 			addCSSRule(document.styleSheets[0], ['sipAction', 'sipStats', 'sipEndpoint'], "display: none !important");
 		}
-		*/
 	}
 	/*
 	$( "#Entry-name" ).on( "focus", function() {
